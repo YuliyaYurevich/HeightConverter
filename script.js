9491373b9ecc3d048f762c8f99e063d581903617
YuliyaYurevich <yurevich@mail.com>
2021-08-20 14:02:03 -0500

    Height Converter

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const feet = document.querySelector('#feet');
+const inches = document.querySelector('#inches');
+const result = document.querySelector('#result');
+
+form.addEventListener('click', (e) => {
+  e.preventDefault();
+  let feetValue = +feet.value;
+  let inchesValue = +inches.value;
+
+  if (
+    feetValue < 0 ||
+    isNaN(feetValue) ||
+    inchesValue < 0 ||
+    isNaN(inchesValue)
+  ) {
+    result.textContent = 'Please enter a valid number';
+  } else {
+    let height = convert(feetValue, inchesValue);
+
+    result.textContent = `${height} cm`;
+  }
+});
+
+function convert(feet, inches) {
+  return (feet * 12 + inches) * 2.54;
+}
